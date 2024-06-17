19c1790f939b62dbb62b399ee16e5bc02d99a382
Anubhav Shukla <114331440+Anubhav1Shukla@users.noreply.github.com>
2024-06-17 21:34:31 +0530

    useState

--- src/Components/UseState.jsx
+++ src/Components/UseState.jsx
@@ -1,16 +1,27 @@
 import React, { useState } from 'react'
 
+//UseState basic intermidiate and advanced
+
 function UseState() {
 
     // const [score,setScore]=useState(100);
-        const [banned,setBanned]=useState(false);
+    // const [banned,setBanned]=useState(false);
+    // const [val,setValue]=useState(0);
+       const [val,setValue]=useState({name:"Anubhav Shukla", isBanned:false});
 
   return (
     <div className='p-4'>
       {/* <h1>{score}</h1> */}
       {/* <button onClick={()=>setScore(200)} className='px-2 py-1 mt-2 rounded-full text-xs bg-blue-500 text-white'>Change</button> */}
-      <h1>{banned.toString()}</h1>
-      <button onClick={()=>setBanned(!banned)} className='px-2 py-1 mt-2 rounded-full text-xs bg-blue-500 text-white'>ban karo</button>
+      {/* <h1>{banned.toString()}</h1> */}
+      {/* <button onClick={()=>setBanned(!banned)} className='px-2 py-1 mt-2 rounded-full text-xs bg-blue-500 text-white'>ban karo</button> */}
+      {/* <h1>{val}</h1>
+      <button onClick={()=>setValue((prev)=>prev+1)} className='px-2 py-1 mt-2 rounded-full text-xs bg-blue-500 text-white '>Add+1</button>
+      <button onClick={()=>setValue((prev)=>prev-1)} className='px-2 py-1 mt-2 rounded-full text-xs bg-blue-500 text-white '>Sub-1</button> */}
+      <h1>name: {val.name}</h1>
+      <h2>banned: {val.isBanned.toString()}</h2>
+      <button onClick={()=>setValue({...val, isBanned: !val.isBanned})} className='px-3 py-1 bg-blue-500 rounded-full text-white'>Change</button>
+
     </div>
   )
 }
