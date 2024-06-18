63eac85a042e6b53e362dec3cd077c4a16496a36
Anubhav Shukla <114331440+Anubhav1Shukla@users.noreply.github.com>
2024-06-18 05:55:28 +0530

    useState Array

--- src/Components/UseState.jsx
+++ src/Components/UseState.jsx
@@ -7,10 +7,11 @@ function UseState() {
     // const [score,setScore]=useState(100);
     // const [banned,setBanned]=useState(false);
     // const [val,setValue]=useState(0);
-       const [val,setValue]=useState({name:"Anubhav Shukla", isBanned:false});
-
+    // const [val,setValue]=useState({name:"Anubhav Shukla", isBanned:false});
+    // const [val,setVal]=useState({name:"Anubhav Shukla", age:20})
+       const [val,setVal]=useState([1,2,3,4,5,6]);
   return (
-    <div className='p-4'>
+    <div className='p-5'>
       {/* <h1>{score}</h1> */}
       {/* <button onClick={()=>setScore(200)} className='px-2 py-1 mt-2 rounded-full text-xs bg-blue-500 text-white'>Change</button> */}
       {/* <h1>{banned.toString()}</h1> */}
@@ -18,10 +19,25 @@ function UseState() {
       {/* <h1>{val}</h1>
       <button onClick={()=>setValue((prev)=>prev+1)} className='px-2 py-1 mt-2 rounded-full text-xs bg-blue-500 text-white '>Add+1</button>
       <button onClick={()=>setValue((prev)=>prev-1)} className='px-2 py-1 mt-2 rounded-full text-xs bg-blue-500 text-white '>Sub-1</button> */}
-      <h1>name: {val.name}</h1>
-      <h2>banned: {val.isBanned.toString()}</h2>
+      {/* <h1>name: {val.name}</h1> */}
+      {/* <h2>banned: {val.isBanned.toString()}</h2> */}
       {/* <button onClick={()=>setValue({...val, isBanned: !val.isBanned})} className='px-3 py-1 bg-blue-500 text-white text-xs rounded-full'>Change</button> */}
-      <button onClick={()=>setValue({...val, isBanned: !val.isBanned})} className={`px-3 py-1 ${val.isBanned ? "bg-blue-500" : "bg-red-500"} rounded-full text-white text-xs`}>Change</button>
+      {/* <button onClick={()=>setValue({...val, isBanned: !val.isBanned})} className={`px-3 py-1 ${val.isBanned ? "bg-blue-500" : "bg-red-500"} rounded-full text-white text-xs`}>Change</button> */}
+
+        {/* UseStatement state ko update karta karta apne hissab se function completion ke baad, to fix performmance issues */}
+
+       {/* <button onClick={()=>{
+         setVal({...val, gender:"male"});
+         console.log(val);
+       }}>Click</button> 
+        */}
+
+       {/* useState Arrays */}
+
+        {val.map(item=><h1>{item*2}</h1>)}
+
+        <button onClick={()=>setVal(()=>val.filter((item,index)=>index!=val.length-1))} className='px-2 py-1 text-ts text-white rounded-full bg-blue-500'>Click</button>
+
     </div>
   )
 }
