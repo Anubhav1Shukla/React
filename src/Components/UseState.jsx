93fe932a08c98181465e380b3bf0530342977e44
Anubhav Shukla <114331440+Anubhav1Shukla@users.noreply.github.com>
2024-06-18 06:25:08 +0530

    useState Array Addition 1

--- src/Components/UseState.jsx
+++ src/Components/UseState.jsx
@@ -45,7 +45,10 @@ function UseState() {
 
         {val.map(item=><h1>{item}</h1>)}
         {/* <button onClick={()=>setVal(()=>val.filter((item,index)=>index!=2))} className='px-2 py-1 text-ts text-white rounded-full bg-blue-500'>Click</button> */}
-        <button onClick={()=>setVal(()=>val.filter(item=> item%2!=0))} className='px-2 py-1 text-ts text-white rounded-full bg-blue-500'>Click</button>
+        {/* <button onClick={()=>setVal(()=>val.filter(item=> item%2!=0))} className='px-2 py-1 text-ts text-white rounded-full bg-blue-500'>Click</button> */}
+
+        <button onClick={()=>setVal([...val,7])} className='px-2 py-1 text-ts text-white rounded-full bg-blue-500'>Click</button>
+
     </div>
   )
 }
