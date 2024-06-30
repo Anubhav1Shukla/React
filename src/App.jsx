f5501aefe3ed56d1593454bfc278769edde8b5cf
Anubhav Shukla <114331440+Anubhav1Shukla@users.noreply.github.com>
2024-06-30 10:58:39 +0530

    Props practice

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,42 @@ import Props from "./Components/Props";
 
 function App(){
 
- 
+
+
+    const data =[
+      {name:"Raj",profession:"Student",image:"https://images.unsplash.com/photo-1688572865439-be9cd43b6986?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", friend:false},
+      {name:"Ravi",profession:"CEO",image:"https://images.unsplash.com/photo-1617330527074-fe659f90e7b5?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",  friend:false},
+      {name:"Sarthak",profession:"IAS",image:"https://images.unsplash.com/photo-1571153041245-5c72320bc914?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",  friend:false},
+      {name:"Sanky",profession:"SP",image:"https://plus.unsplash.com/premium_photo-1686803469619-24c33c76c340?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",  friend:false}
+    ];
+    
+    const[realdata,setRealData]=useState(data);
+    //state jaha banati hai vahi modify hoti hai
+  const handleFriendsButton=(cardindex)=>{
+    setRealData((prev)=>{
+      return prev.map((item,index)=>{
+        if (index === cardindex) {
+            return { ...item, friend: !item.friend };
+        }
+         return item;
+      })
+    })
+  }
   return (
     <>
      
 
       {/* <Card/> */}
       {/* <UseState/>  */}
-      <Props text="Know More" color="bg-blue-500"/>
-      <Props text="Download..." color="bg-red-500"/>
-      
+      {/* <Props text="Know More" color="bg-blue-500"/> */}
+      {/* <Props text="Download..." color="bg-red-500"/>
+      <Props text="Feedback" color="bg-yellow-500"/> */}
+
+        <div className="w-full h-screen bg-zinc-300 flex gap-3 items-center justify-center">
+      {realdata.map((item,index)=>(
+        <Props key={index} image={item.image} name={item.name} friend={item.friend} profession={item.profession} handleClick={()=>handleFriendsButton(index)}/>
+      ))}
+      </div>
     
     </>
   );
--- src/Components/Props.jsx
+++ src/Components/Props.jsx
@@ -1,12 +1,29 @@
 import React from "react";
 
-function Props({text,color}) {
+function Props({index,handleClick,name,image,profession,friend}) {
+    // const {name,image,profession}=values;
   return (
-        <button className={`m-2 px-3 py-1 text-zinc-100 text-xs ${color} rounded-md`}>{text}</button>
-  )
+        // <button className={`m-2 px-3 py-1 text-zinc-100 text-xs ${color} rounded-md`}>{text}</button>
+        <div className="w-52 bg-white rounded-md overflow-hidden">
+            <div className="w-full h-44 bg-sky-300">
+                <img className="w-full h-full object-cover " src={image} alt="" />
+            </div>
+            <div className="w-full p-3">
+                <h3 className="text-xl font-semibold">{name}</h3>
+                <h5 className="text-xs">{profession}</h5>
+                {/* props drilling event handling */}
+                <button onClick={handleClick} className="mt-4 px-10 py-1 text-xs text-white bg-blue-500 font-semibold rounded-md">{friend ===true ?"Friends" : "Add Friend"}</button>
+            </div>
+        </div>
+  );
 }
 
 export default Props;
 
 //props introduction:-
 //props use karte hai aapke componenets ko reusbale banane ke liye , consider kare aapke pass ek button hai and aapko aapkko us button ko alag alag jagah daalna hai aap mein , to app ek button component bannaye and uska data hard coded karne ki jagah parent component se send karde and child component par use kar le
+
+
+
+// practice 
+// humara data actually main app componenet main hoga aur hum use props ke through pass karenge cards mein , har card par ek button hoga and hamein add friend button par click hone par alert dena hai
