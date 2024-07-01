f5fabd493ad27202dbbdd8e20cb8594f0fbe0335
Anubhav Shukla <114331440+Anubhav1Shukla@users.noreply.github.com>
2024-07-01 09:03:32 +0530

    module css

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import styles from './style.module.css';
+
+
 
 function Navbar({data}) {
   return (
     <div className='w-full px-20 py-3 flex justify-between item'>
-      <h3>Music App</h3>
+      <h3 className={styles.a}>Music App</h3>
       <div className='flex p-2 gap-3 px-4 bg-orange-500 text-white rounded-md'>
       <h3>Favourites</h3>
         <h4>{data.filter(item=>item.added).length}</h4>
