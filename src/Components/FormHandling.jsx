4b11436fa3b5b6b704647351ec98b22dce1296f9
Anubhav Shukla <114331440+Anubhav1Shukla@users.noreply.github.com>
2024-07-01 12:27:56 +0530

    form handling by Controlled Component

--- src/Components/FormHandling.jsx
+++ src/Components/FormHandling.jsx
@@ -1,21 +1,47 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 
 function FormHandling() {
-    const name=useRef(null);
-    const age=useRef(null);
-    const email=useRef(null);
-    const handleSubmit =(details)=>{
-        details.preventDefault();
-        console.log(name.current.value,age.current.value,email.current.value,email.current.value)
-    }
+
+            //UseReference
+    // const name=useRef(null);
+    // const age=useRef(null);
+    // const email=useRef(null);
+    // const handleSubmit =(details)=>{
+    //     details.preventDefault();
+    //     console.log(name.current.value,age.current.value,email.current.value,email.current.value)
+    // }
+
+           //controlled components
+    // const [val,setVal]=useState({name:" ", email:""})
+
+    // const handleSubmit =(details)=>{
+    //     details.preventDefault();
+    //     console.log(val);
+    // }
+                    //Hook
 
   return (
-    <form action="" onSubmit={handleSubmit}>
-        <input ref={name} type="text"  placeholder='name' />
-        <input ref={age} type="text" placeholder='age' />
-        <input ref={email} type="text" placeholder='Email' />
-        <input type="Submit" />
-    </form>
+                //UseRefrence
+    // <form action="" onSubmit={handleSubmit}>
+    //     <input ref={name} type="text"  placeholder='name' />
+    //     <input ref={age} type="text" placeholder='age' />
+    //     <input ref={email} type="text" placeholder='Email' />
+    //     <input type="Submit" />
+    // </form>
+
+        //Controlled components
+        // <form action="" onSubmit={handleSubmit}>
+        //     <input onChange={(event)=>setVal({...val,name:event.target.value})} type="text" placeholder='name' />
+        //     <input onChange={(event)=>setVal({...val,email:event.target.value})} type="email" placeholder='email' />
+        //     <input type="Submit" />
+        // </form>
+
+
+        //Hook
+        <form action="">
+            
+        </form>
+
   )
 }
 
@@ -27,5 +53,5 @@ export default FormHandling
 // form submission par website reload ho jati hai aur react ka main focus ye hai ki webpage reload nahi hona chaiye varna kya fayada react ka
 // three tyes to handle form reloading
 // useRef = is tareeke mein hum har input ko select kar lete hai and unki value tab nikaalte hai jab form submit hota hai. useRef ke through kisi bhi html input ko select kar sakte ho
-// controlled componenets
+// controlled componenets = aap jab bhi kuch likhe useState ke through data real time par update kar dein jaise hi kuch type ho set state kar do nayi value ke barabar
 // react hook form
