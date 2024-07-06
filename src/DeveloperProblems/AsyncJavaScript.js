3effec9cd395fa6fd117eb36cc34aafad2c9dae1
Anubhav Shukla <114331440+Anubhav1Shukla@users.noreply.github.com>
2024-07-06 21:25:31 +0530

    setInterval

--- src/DeveloperProblems/AsyncJavaScript.js
+++ src/DeveloperProblems/AsyncJavaScript.js
@@ -20,7 +20,7 @@
 // setinterval:-->Setinterval ka code kuchh der baad chalta baar baar ek particular interval time mein
 // setInterval(() => {
 //     console.log("Anubhav");
-// },1000);
+// },2000);
 
 // Fetch API:-->Ye kisi aur url(API) par jaa kar kuchh der data laayega ya data humaare pass se us url(API) par lekar jaayega
 //kyuki ye internet par jayega aur fir data ko lekkar aayega to isme time lagta hai to by default hi js mein fetch ko async banaya gaya hai kyuki fetch ka kaam hai data laana wo bhi kisi url se an aisa ho sakta hai us url ki website slow ho aur agar fetch sync hota uske baad ka code tab tak nahi chalata jab uska data nahi jayega jiske karad problem hota
